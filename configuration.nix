eee1dc3bbe83e317f046c010614d24ad83f35abb
Sebastião Giacheto Ferreira Júnior <sebastorama@gmail.com>
2024-07-05 15:03:20 -0300

    Adds homebrew and font config

--- configuration.nix
+++ configuration.nix
@@ -4,19 +4,33 @@
   # $ nix-env -qaP | grep wget
   environment.systemPackages = with pkgs; [
     vim
-    kitty
   ];
+
+  homebrew = {
+    enable = true;
+    casks = [
+      "1password"
+      "alfred"
+      "discord"
+      "dropbox"
+      "google-chrome"
+      "jetbrains-toolbox"
+      "karabiner-elements"
+      "kitty"
+      "parallels"
+      "whatsapp"
+    ];
+    onActivation.autoUpdate = true;
+    onActivation.upgrade = true;
+  };
   
-  # Auto upgrade nix package and the daemon service.
   services.nix-daemon.enable = true;
   # nix.package = pkgs.nix;
   
-  # Necessary for using flakes on this system.
   nix.settings.experimental-features = "nix-command flakes";
   
   # Create /etc/zshrc that loads the nix-darwin environment.
   programs.zsh.enable = true;  # default shell on catalina
-  # programs.fish.enable = true;
   
   # Set Git commit hash for darwin-version.
   system.configurationRevision = self.rev or self.dirtyRev or null;
@@ -24,6 +38,22 @@
   # Used for backwards compatibility, please read the changelog before changing.
   # $ darwin-rebuild changelog
   system.stateVersion = 4;
+
+  fonts.packages = with pkgs; [
+   (nerdfonts.override { fonts = [ "JetBrainsMono" ]; })
+  ];
+
+  system.defaults.dock.autohide = true;
+  system.keyboard.enableKeyMapping = true;
+  system.keyboard.remapCapsLockToControl = true;
+
+  system.defaults.NSGlobalDomain = {
+    ApplePressAndHoldEnabled = false;
+    KeyRepeat = 2;
+    InitialKeyRepeat = 15;
+  };
+
+  security.pam.enableSudoTouchIdAuth = true;
   
   # The platform the configuration will be used on.
   nixpkgs.hostPlatform = system;
--- home.nix
+++ home.nix
@@ -40,8 +40,6 @@
     stdenv
     tldr
     tmux
-    (nerdfonts.override { fonts = [ "JetBrainsMono" "IntelOneMono" ]; })
-
     # # It is sometimes useful to fine-tune packages, for example, by applying
     # # overrides. You can do that directly here, just don't forget the
     # # parentheses. Maybe you want to install Nerd Fonts with a limited number of
