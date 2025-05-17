eadef6b802cc68ad83acf4b25a005de1c7aefb35
Sebastião Giacheto Ferreira Júnior <sebastorama@gmail.com>
2025-05-17 19:34:20 -0400

    nix flake update (home-manager version mismatch for now)

--- configuration.nix
+++ configuration.nix
@@ -97,6 +97,8 @@
   # Create /etc/zshrc that loads the nix-darwin environment.
   programs.zsh.enable = true;  # default shell on catalina
 
+  system.primaryUser = "sebastorama";
+
   # Set Git commit hash for darwin-version.
   system.configurationRevision = self.rev or self.dirtyRev or null;
 
